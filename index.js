1ab2abf2cb982d02971d472db93db0c79ecd82bb
jjwernerm <joannywerner@gmail.com>
2023-10-06 00:23:25 -0300

    my 7th commit

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+res.header('Access-Control-Allow-Origin', 'https://dancing-crumble-2291c4.netlify.app'); // Reemplaza con la URL correcta de tu front-end
+res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Agrega los métodos que tu aplicación utiliza
+res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+// También puedes configurar Access-Control-Allow-Credentials si es necesario
+res.header('Access-Control-Allow-Credentials', 'true');
+
+// Luego envía la respuesta
+res.status(200).send('OK');
+
+
 const express = require('express');
 const cors = require('cors'); // Importa el middleware CORS
 
