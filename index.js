0946f051a24da45175a2c8260971cb1036f72b38
jjwernerm <joannywerner@gmail.com>
2023-10-06 00:04:26 -0300

    my 5th commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors'); // Importa el middleware CORS
 
 const app = express();
-app.use(cors({ origin: 'https://dancing-crumble-2291c4.netlify.app/' })); // Habilita CORS para todas las rutas
+app.use(cors({ origin: 'https://dancing-crumble-2291c4.netlify.appp/' })); // Habilita CORS para todas las rutas
 app.use(express.json())
 
 // Importar las rutas desde el archivo "empleadosRoutes.js"
