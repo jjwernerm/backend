d82628d25afb49d7c930a5a20fc14aee539028bc
jjwernerm <joannywerner@gmail.com>
2023-10-08 20:43:49 -0300

    --

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ const express = require('express');
 const cors = require('cors'); // Importa el middleware CORS
 
 const app = express();
-app.use(cors({ origin: 'https://dancing-crumble-2291c4.netlify.app/' })); // Habilita CORS para todas las rutas
-app.options('*', cors()); // Manejar todas las solicitudes OPTIONS
-
+app.use(cors()); // Habilita CORS para todas las rutas
 app.use(express.json())
 
 // Importar las rutas desde el archivo "empleadosRoutes.js"
@@ -12,5 +10,5 @@ const configureRoutes = require('./routes/empleadoRoutes');
 configureRoutes(app);
 
 app.listen(3000, () => {
-  console.log("SERVIDOR ENCENDIDO OK");
+  console.log("SERVIDOR ENCENDIDO");
 });
